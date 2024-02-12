33f59130603b9614aee4a630ba5e4bf99ec7fbaa
Piyush Pardeshi <piyushp0512@gmail.com>
2024-02-13 02:04:34 +0530

    light and dark theme added in login and signup page, themeProvider added

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -7,32 +7,43 @@ const Login = () => {
   return isAuthenticated ? (
     <Navigate to="/" />
   ) : (
-    <div className="w-full h-[100vh]">
+    <div className="w-full h-[100vh] bg-bgLight dark:bg-bgDark">
       <img
         src="https://static.cdninstagram.com/rsrc.php/yH/r/JesX5Buw5jT.webp"
         alt="..."
-        className="w-[95%] absolute top-0 left-[50%] -translate-x-1/2 z-[-1] md:block hidden"
+        className="w-[95%] absolute top-0 left-[50%] -translate-x-1/2 md:block hidden"
       />
-      <div className="w-full h-[100vh] flex justify-center items-center z-10">
+      <div className="w-full h-[100vh] flex justify-center items-center relative z-10">
         <div className="flex flex-col justify-center items-center gap-5 max-w-[400px] w-[100%] p-[24px] placeholder-fuchsia-100">
-          <h2 className="text-3xl font-bold">Log in to vichar</h2>
+          <h2 className="text-3xl font-bold text-textLight dark:text-textDark ">
+            Log in to vichar
+          </h2>
           <form className="flex flex-col gap-5 w-[100%]">
             <input
               type="text"
               placeholder="Username, Email or Phone number"
-              className="w-[100%] py-4 px-3 text-[13px] rounded-xl bg-gray-100 focus:outline-gray-500 focus:outline focus: outline-1"
+              className="w-[100%] py-4 px-3 text-[13px] rounded-xl focus:outline outline-1 bg-inputColorLight dark:bg-inputColorDark dark:text-textDark
+              focus:outline-primaryLight dark:focus:outline-primaryDark
+              "
             />
             <input
               type="password"
               placeholder="Password"
-              className="w-[100%] py-4 px-3 text-[13px] rounded-xl bg-gray-100 focus:outline-gray-500 focus:outline focus: outline-1"
+              autoComplete="new-password"
+              className="w-[100%] py-4 px-3 text-[13px] rounded-xl focus:outline outline-1 bg-inputColorLight dark:bg-inputColorDark dark:text-textDark
+              focus:outline-primaryLight dark:focus:outline-primaryDark"
             />
-            <button className="w-[100%] py-4 px-3 rounded-xl bg-black text-white">
+            <button
+              className="w-[100%] py-4 px-3 rounded-xl 
+              bg-[#101010] hover:bg-[#202020] transition dark:bg-gradient-to-r dark:from-teal-400 dark:to-blue-300 dark:hover:from-teal-500 dark:hover:to-blue-400 text-textDark dark:text-textLight"
+            >
               Login
             </button>
           </form>
-          <p className="text-[13px] cursor-pointer">Forgotten password?</p>
-          <p className="text-[13px]">
+          <p className="text-[13px] cursor-pointer text-textLight dark:text-textDark">
+            Forgotten password?
+          </p>
+          <p className="text-[13px] text-textLight dark:text-textDark">
             Don't have an account?{" "}
             <Link to={"/signup"} className="font-bold cursor-pointer">
               Sign up
--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -56,15 +56,17 @@ const Signup = () => {
   return isAuthenticated ? (
     <Navigate to="/" />
   ) : (
-    <div className="w-full h-[100vh]">
+    <div className="w-full h-[100vh] bg-bgLight dark:bg-bgDark">
       <img
         src="https://static.cdninstagram.com/rsrc.php/yH/r/JesX5Buw5jT.webp"
         alt="..."
-        className="w-[95%] absolute top-0 left-[50%] -translate-x-1/2 z-[-1] md:block hidden"
+        className="w-[95%] absolute top-0 left-[50%] -translate-x-1/2 md:block hidden"
       />
-      <div className="w-full h-[100vh] flex justify-center items-center z-10">
+      <div className="w-full h-[100vh] flex justify-center items-center relative z-10">
         <div className="flex flex-col justify-center items-center gap-5 max-w-[400px] mt-0 md:mt-[150px] w-[100%] p-[24px] placeholder-fuchsia-100">
-          <h2 className="text-3xl font-bold">Signup to vichar</h2>
+          <h2 className="text-3xl font-bold text-textLight dark:text-textDark">
+            Signup to vichar
+          </h2>
           <form className="flex flex-col gap-5 w-[100%]" autoComplete="off">
             <div className="relative">
               <div className="absolute top-[50%] right-[15px] -translate-y-1/2 text-[20px]">
@@ -79,7 +81,8 @@ const Signup = () => {
               <input
                 type="text"
                 placeholder="Email or Phone number"
-                className={`w-[100%] py-4 px-3 text-[13px] rounded-xl bg-gray-100 focus:outline-gray-500 focus:outline focus:outline-1 ${
+                className={`w-[100%] py-4 px-3 text-[13px] rounded-xl focus:outline outline-1 bg-inputColorLight dark:bg-inputColorDark dark:text-textDark
+                focus:outline-primaryLight dark:focus:outline-primaryDark ${
                   validEmailOrPhone === false ? "focus:outline-red-500" : ""
                 }`}
                 onChange={handleEmailOrPhoneChange}
@@ -88,7 +91,8 @@ const Signup = () => {
             <input
               type="text"
               placeholder="Full name"
-              className="w-[100%] py-4 px-3 text-[13px] rounded-xl bg-gray-100 focus:outline-gray-500 focus:outline focus: outline-1"
+              className="w-[100%] py-4 px-3 text-[13px] rounded-xl focus:outline outline-1 bg-inputColorLight dark:bg-inputColorDark dark:text-textDark
+                focus:outline-primaryLight dark:focus:outline-primaryDark"
             />
             <div className="relative">
               <div className="absolute top-[50%] right-[15px] -translate-y-1/2 text-[20px]">
@@ -103,7 +107,8 @@ const Signup = () => {
               <input
                 type="text"
                 placeholder="Username"
-                className="w-[100%] py-4 px-3 text-[13px] rounded-xl bg-gray-100 focus:outline-gray-500 focus:outline focus: outline-1"
+                className="w-[100%] py-4 px-3 text-[13px] rounded-xl focus:outline outline-1 bg-inputColorLight dark:bg-inputColorDark dark:text-textDark
+                focus:outline-primaryLight dark:focus:outline-primaryDark"
                 maxLength={20}
                 onChange={handleUsernameChange}
               />
@@ -112,14 +117,19 @@ const Signup = () => {
               <input
                 type="password"
                 placeholder="Password"
-                className="w-[100%] py-4 px-3 text-[13px] rounded-xl bg-gray-100 focus:outline-gray-500 focus:outline focus: outline-1"
+                autoComplete="new-password"
+                className="w-[100%] py-4 px-3 text-[13px] rounded-xl focus:outline outline-1 bg-inputColorLight dark:bg-inputColorDark dark:text-textDark
+                focus:outline-primaryLight dark:focus:outline-primaryDark"
               />
             </div>
-            <button className="w-[100%] py-4 px-3 rounded-xl bg-black text-white">
+            <button
+              className="w-[100%] py-4 px-3 rounded-xl 
+              bg-[#101010] hover:bg-[#202020] transition dark:bg-gradient-to-r dark:from-teal-400 dark:to-blue-300 dark:hover:from-teal-500 dark:hover:to-blue-400 text-textDark dark:text-textLight"
+            >
               Signup
             </button>
           </form>
-          <p className="text-[13px]">
+          <p className="text-[13px] cursor-pointer text-textLight dark:text-textDark">
             Allready have an account?{" "}
             <Link to={"/login"} className="font-bold cursor-pointer">
               Login
